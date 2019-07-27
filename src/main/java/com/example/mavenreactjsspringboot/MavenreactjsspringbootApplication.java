876f6eb087292c424dc456e379118d9fc4e8befd
Mike Møller Nielsen <mmni@protonmail.com>
2019-07-27 18:29:13 +0200

    added files

---
+++ src/main/java/com/example/mavenreactjsspringboot/MavenreactjsspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenreactjsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenreactjsspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenreactjsspringbootApplication.class, args);
+    }
+
+}
